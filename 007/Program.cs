0a91ed163dc350d8d9bc7dd89c3d84078fc51ff7
Oksana <oksanasolo@gmail.com>
2023-01-29 20:57:11 +0300

    Lesson102

---
+++ 007/Program.cs
@@ -0,0 +1,13 @@
+﻿// 7.Выяснить является ли число чётным.
+int N2=7;
+bool odd=N2%2==0;
+if (odd==true)
+{
+    System.Console.Write ($"Число {N2} четное");
+}
+else
+{
+    System.Console.Write ($"Число {N2} не является четным");
+}
+
+
