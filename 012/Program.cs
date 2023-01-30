ddfbc25025a8a1a6c0c689ab34e197a5727b50d6
Oksana <oksanasolo@gmail.com>
2023-01-30 20:14:15 +0300

    Lesson104

---
+++ 012/Program.cs
@@ -0,0 +1,23 @@
+﻿// 12. С клавиатуры вводится целое число из  диапазона  [10, 99]. Показать наибольшую цифру числа
+int a;
+System.Console.WriteLine ("Введите число a:");
+a=Convert.ToInt32 (Console.ReadLine());
+int N = 0;
+if (a<=99 && a>=10)
+{
+    N=a;
+
+}
+else
+{
+    Console.WriteLine ("Заднное число не входит в диапозон");
+}
+int d0,d1,max;
+d0=N%10;
+d1=N/10%10;
+max=d0;
+if (d0<d1)
+{
+    max=d1;
+}
+Console.WriteLine (max);
